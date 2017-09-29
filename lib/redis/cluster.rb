5bd4fe18461ac6bf2e25dcbada32f1bddc53efc6
Taishi Kasuga <kasuga@livesense.co.jp>
2017-09-29 13:19:28 +0900

    Add cluster command support

--- lib/redis/cluster.rb
+++ lib/redis/cluster.rb
@@ -4,32 +4,71 @@ class Redis
   # Copyright (C) 2013 Salvatore Sanfilippo <antirez@gmail.com>
   # https://github.com/antirez/redis-rb-cluster
   # TODO: Failover consideration
-  # TODO: M* commands consideration
   # TODO: Asking consideration
-  # TODO: CLUSTER commands support
   class Cluster
     def initialize(node_configs, options = {})
       raise ArgumentError, 'Redis Cluster node config must be Array' unless node_configs.is_a?(Array)
 
-      @slot_node_key_maps = {}
-      @nodes = node_configs.map do |config|
-        option = to_client_option(config)
-        [to_node_key(option), Redis.new(options.merge(option))]
-      end.to_h
+      startup_nodes = build_clients_per_node(node_configs, options)
+      available_slots = fetch_available_slots_per_node(startup_nodes.values)
+
+      raise CannotConnectError, 'Could not connect to any nodes' if available_slots.nil?
+
+      available_node_urls = available_slots.keys.map { |ip_port| "redis://#{ip_port}" }
+      @available_nodes = build_clients_per_node(available_node_urls, options)
+      @slot_node_key_maps = build_slot_node_key_maps(available_slots)
+    end
+
+    def cluster(command, *args)
+      response = find_node.cluster(command, *args)
+      case command.to_s.downcase
+      when 'slots' then cluster_slots(response)
+      when 'nodes' then cluster_nodes(response)
+      when 'slaves' then cluster_slaves(response)
+      when 'info' then cluster_info(response)
+      else response
+      end
     end
 
     private
 
     def respond_to_missing?(method_name, _include_private = false)
-      @nodes.values.first.respond_to?(method_name)
+      find_node.respond_to?(method_name)
     end
 
     def method_missing(method_name, *args)
       key = extract_key(args)
       slot = KeySlotConverter.convert(key)
-      node = select_node(slot)
+      node = find_node(slot)
       return try_cmd(node, method_name, *args) if node.respond_to?(method_name)
       super
+    end
+
+    def find_node(slot = nil)
+      return @available_nodes.values.sample if slot.nil? || !@slot_node_key_maps.key?(slot)
+
+      node_key = @slot_node_key_maps[slot]
+      @available_nodes.fetch(node_key)
+    end
+
+    def try_cmd(node, command, *args)
+      node.send(command, *args)
+    rescue CommandError => err
+      raise err unless err.message.start_with?('MOVED')
+      redirection_node(err.message).send(command, *args)
+    end
+
+    def redirection_node(err_msg)
+      _, slot, node_key = err_msg.split(' ')
+      @slot_node_key_maps[slot.to_i] = node_key
+      find_node(slot.to_i)
+    end
+
+    def build_clients_per_node(node_configs, options)
+      node_configs.map do |config|
+        option = to_client_option(config)
+        [to_node_key(option), Redis.new(options.merge(option))]
+      end.to_h
     end
 
     def to_client_option(config)
@@ -49,6 +88,36 @@ class Redis
       "#{option[:host]}:#{option[:port]}"
     end
 
+    def fetch_available_slots_per_node(startup_nodes)
+      slot_info = nil
+
+      startup_nodes.each do |node|
+        begin
+          slot_info = fetch_slot_info(node)
+        rescue CannotConnectError
+          next
+        end
+
+        break
+      end
+
+      slot_info
+    end
+
+    def fetch_slot_info(node)
+      node.cluster('slots').map do |slot_info|
+        first_slot, last_slot = slot_info[0..1]
+        ip, port = slot_info[2]
+        ["#{ip}:#{port}", (first_slot..last_slot)]
+      end.to_h
+    end
+
+    def build_slot_node_key_maps(available_slots)
+      available_slots.each_with_object({}) do |(node_key, slots), m|
+        slots.each { |slot| m[slot] = node_key }
+      end
+    end
+
     def extract_key(args)
       key = args.first.to_s
       hash_tag = extract_hash_tag(key)
@@ -64,29 +133,46 @@ class Redis
       key[s + 1..e - 1]
     end
 
-    def select_node(slot)
-      return @nodes.values.sample unless @slot_node_key_maps.key?(slot)
-
-      node_key = @slot_node_key_maps[slot]
-      @nodes.fetch(node_key)
+    def convert_serialized_node_info_into_hash(str)
+      arr = str.split(' ')
+      {
+        node_id: arr[0],
+        ip_port: arr[1],
+        flags: arr[2].split(','),
+        master_node_id: arr[3],
+        ping_sent: arr[4],
+        pong_recv: arr[5],
+        config_epoch: arr[6],
+        link_state: arr[7],
+        slots: arr[8].nil? ? nil : Range.new(*arr[8].split('-'))
+      }
     end
 
-    def try_cmd(node, command, *args)
-      node.send(command, *args)
-    rescue Redis::CommandError => err
-      msg = err.message
-      return destination_node(msg).send(command, *args) if redirection_needed?(msg)
-      raise err
+    def cluster_slots(response)
+      response.map do |res|
+        first_slot, last_slot = res[0..1]
+        master = { ip: res[2][0], port: res[2][1], node_id: res[2][2] }
+        replicas = res[3..-1].map { |r| { ip: r[0], port: r[1], node_id: r[2] } }
+        { start_slot: first_slot, end_slot: last_slot, master: master, replicas: replicas }
+      end
     end
 
-    def redirection_needed?(err_msg)
-      err_msg.start_with?('MOVED')
+    def cluster_nodes(response)
+      response
+        .split(/[\r\n]+/)
+        .map { |node_info| convert_serialized_node_info_into_hash(node_info) }
     end
 
-    def destination_node(err_msg)
-      _, slot, node_key = err_msg.split(' ')
-      @slot_node_key_maps[slot.to_i] = node_key
-      @nodes.fetch(node_key)
+    def cluster_slaves(response)
+      response.map { |node_info| convert_serialized_node_info_into_hash(node_info) }
+    end
+
+    def cluster_info(response)
+      response
+        .split(/[\r\n]+/)
+        .map { |str| str.split(':') }
+        .map { |arr| [arr.first.to_sym, arr[1]] }
+        .to_h
     end
   end
 end
--- test/cluster_test.rb
+++ test/cluster_test.rb
@@ -16,6 +16,70 @@ class TestCluster < Test::Unit::TestCase
     assert_equal 'bar', @r.send(:extract_hash_tag, 'foo{bar}{zap}')
   end
 
+  def test_cluster_slots
+    nodes = (7000..7005).map { |port| "redis://127.0.0.1:#{port}" }
+
+    @r = Redis::Cluster.new(nodes)
+
+    assert_equal 3, @r.cluster('slots').length
+    assert_equal true, @r.cluster('slots').first.key?(:start_slot)
+    assert_equal true, @r.cluster('slots').first.key?(:end_slot)
+    assert_equal true, @r.cluster('slots').first.key?(:master)
+    assert_equal true, @r.cluster('slots').first.fetch(:master).key?(:ip)
+    assert_equal true, @r.cluster('slots').first.fetch(:master).key?(:port)
+    assert_equal true, @r.cluster('slots').first.fetch(:master).key?(:node_id)
+    assert_equal true, @r.cluster('slots').first.key?(:replicas)
+    assert_equal true, @r.cluster('slots').first.fetch(:replicas).is_a?(Array)
+    assert_equal true, @r.cluster('slots').first.fetch(:replicas).first.key?(:ip)
+    assert_equal true, @r.cluster('slots').first.fetch(:replicas).first.key?(:port)
+    assert_equal true, @r.cluster('slots').first.fetch(:replicas).first.key?(:node_id)
+  end
+
+  def test_cluster_keyslot
+    nodes = (7000..7005).map { |port| "redis://127.0.0.1:#{port}" }
+
+    @r = Redis::Cluster.new(nodes)
+
+    assert_equal Redis::Cluster::KeySlotConverter.convert('hogehoge'), @r.cluster('keyslot', 'hogehoge')
+    assert_equal Redis::Cluster::KeySlotConverter.convert('12345'), @r.cluster('keyslot', '12345')
+    assert_equal Redis::Cluster::KeySlotConverter.convert('foo'), @r.cluster('keyslot', 'boo{foo}woo')
+    assert_equal Redis::Cluster::KeySlotConverter.convert('antirez.is.cool'), @r.cluster('keyslot', 'antirez.is.cool')
+  end
+
+  def test_cluster_nodes
+    nodes = (7000..7005).map { |port| "redis://127.0.0.1:#{port}" }
+
+    @r = Redis::Cluster.new(nodes)
+
+    assert_equal 6, @r.cluster('nodes').length
+    assert_equal true, @r.cluster('nodes').first.key?(:node_id)
+    assert_equal true, @r.cluster('nodes').first.key?(:ip_port)
+    assert_equal true, @r.cluster('nodes').first.key?(:flags)
+    assert_equal true, @r.cluster('nodes').first.key?(:master_node_id)
+    assert_equal true, @r.cluster('nodes').first.key?(:ping_sent)
+    assert_equal true, @r.cluster('nodes').first.key?(:pong_recv)
+    assert_equal true, @r.cluster('nodes').first.key?(:config_epoch)
+    assert_equal true, @r.cluster('nodes').first.key?(:link_state)
+    assert_equal true, @r.cluster('nodes').first.key?(:slots)
+  end
+
+  def test_cluster_slaves
+    nodes = (7000..7005).map { |port| "redis://127.0.0.1:#{port}" }
+
+    @r = Redis::Cluster.new(nodes)
+
+    sample_master_node_id = @r.cluster('nodes').find { |n| n.fetch(:master_node_id) == '-' }.fetch(:node_id)
+    assert_equal 'slave', @r.cluster('slaves', sample_master_node_id).first.fetch(:flags).first
+  end
+
+  def test_cluster_info
+    nodes = (7000..7005).map { |port| "redis://127.0.0.1:#{port}" }
+
+    @r = Redis::Cluster.new(nodes)
+
+    assert_equal '3', @r.cluster('info').fetch(:cluster_size)
+  end
+
   def test_well_known_commands_work
     nodes = ['redis://127.0.0.1:7000',
              'redis://127.0.0.1:7001',
@@ -56,20 +120,24 @@ class TestCluster < Test::Unit::TestCase
   def test_client_does_not_accept_db_specified_url
     nodes = ['redis://127.0.0.1:7000/1/namespace']
 
-    @r = Redis::Cluster.new(nodes)
-
     assert_raise(Redis::CommandError, 'ERR SELECT is not allowed in cluster mode') do
-      @r.set('key', 'value')
+      @r = Redis::Cluster.new(nodes)
     end
   end
 
-  def test_client_does_not_accept_unconnectable_node_url
+  def test_client_does_not_accept_unconnectable_node_url_only
     nodes = ['redis://127.0.0.1:7006']
 
-    @r = Redis::Cluster.new(nodes)
+    assert_raise(Redis::CannotConnectError, 'Could not connect to any nodes') do
+      @r = Redis::Cluster.new(nodes)
+    end
+  end
 
-    assert_raise(Redis::CannotConnectError) do
-      @r.set('hogehoge', 1)
+  def test_client_accept_unconnectable_node_url_included
+    nodes = ['redis://127.0.0.1:7000', 'redis://127.0.0.1:7006']
+
+    assert_nothing_raised(Redis::CannotConnectError, 'Could not connect to any nodes') do
+      @r = Redis::Cluster.new(nodes)
     end
   end
 
@@ -77,14 +145,6 @@ class TestCluster < Test::Unit::TestCase
     nodes = ['http://127.0.0.1:80']
 
     assert_raise(ArgumentError, "invalid uri scheme 'http'") do
-      @r = Redis::Cluster.new(nodes)
-    end
-  end
-
-  def test_client_does_not_accept_unix_scheme_url
-    nodes = ['unix://tmp/redis.sock']
-
-    assert_nothing_raised(ArgumentError, "invalid uri scheme 'unix'") do
       @r = Redis::Cluster.new(nodes)
     end
   end
